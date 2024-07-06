d3442181867b5b8d0a9392909a233815e6c9db32
“BrandC” <brandc@example.com>
2024-07-06 12:43:46 -0600

    Renamed URL downloader function

--- GoRSS.go
+++ GoRSS.go
@@ -1,35 +1,54 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
-//	"log"
+//	"log/slog"
 //	"time"
 
-//	"net/http"
+	"net/http"
 )
 
 // Command line variables
-var URLLink string
+var RSSLink string
 var OutputDir string
 
 func init() {
-	flag.StringVar(&URLLink, "i", "", "Input URL")
+	flag.StringVar(&RSSLink,   "r", "", 		   "RSS Link")
 	flag.StringVar(&OutputDir, "o", "./GoRSSDefault/", "Output Directory")
 	flag.Parse()
 }
 
+func URLDownload(URL string) (data []byte, err error) {
+	Response, err := http.Get(URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "")
+		return nil, errors.Join(err, " -> http.Get/RSSFeedDownloader/")
+	}
+	defer Response.Body.Close()
+
+	if Response.StatusCode != 200 {
+		fmt.Fprintf(os.Stderr, "Status code: %s\n", Response.Status)
+		return nil, errors.Join("Response did not contain '200 OK'")
+	}
+
+	data = io.ReadAll(Response.Body)
+	return data, nil
+}
+
 func main() {
 	// Sanity checks of command line input.
-	if len(URLLink) == 0 {
-		fmt.Fprintln(os.Stderr, "Please specify a link with '-i'.")
+	if len(RSSLink) == 0 {
+		fmt.Fprintln(os.Stderr, "Please specify a link with '-r'.")
 		return
 	}
 
 	// Display command line input.
-	fmt.Printf("URLLink: %s\n", URLLink)
-	fmt.Printf("OutputDir: %s\n", OutputDir)
+	fmt.Printf("RSS feed: %s\n", RSSLink)
+	fmt.Printf("Output directory: \"%s\"\n", OutputDir)
 
 	return
 }
